7d460beef884d36717915e08317dd610c324beb5
BramVanroy <Bram.Vanroy@UGent.be>
2021-08-24 10:47:15 +0200

    add link to demo

--- astred-demo-frontend/src/components/PageFooter.js
+++ astred-demo-frontend/src/components/PageFooter.js
@@ -8,7 +8,7 @@ function PageFooter() {
     <footer id="page-footer">
       <p>Built by <a href="http://bramvanroy.be" title="Home page Bram Vanroy" target="_blank" rel="noreferrer noopener">Bram Vanroy</a> in light of his completed
              PhD project <a href="https://research.flw.ugent.be/en/projects/predict" title="Predicting difficulty in translation home page" target="_blank" rel="noreferrer noopener">PreDicT</a>.</p>
-      <p>The code for this demo is available on Github. It makes use of the <a href="https://github.com/BramVanroy/astred" title="ASTrED Python library" target="_blank" rel="noreferrer noopener">ASTrED</a> library.</p>
+      <p>The code for this demo is <a href="https://github.com/BramVanroy/astred-demo" title="ASTrED demo source code" target="_blank" rel="noreferrer noopener">available on Github</a>. It makes use of the <a href="https://github.com/BramVanroy/astred" title="ASTrED Python library" target="_blank" rel="noreferrer noopener">ASTrED</a> library.</p>
     </footer>
   );
 }
